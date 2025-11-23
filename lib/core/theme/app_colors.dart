d329e5462ff97bb40f53d88c5ed5813d18743af8
Nour <nourhanaymanghonim@gmail.com>
2025-11-23 18:05:40 +0200

    feat: Add initial project structure

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff75A460);
+  static const Color darkGray = Color(0xff4C4F54);
+  static const Color red = Color(0xffEF2D28);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color yellow = Color(0xffFFD628);
+  static const Color grey = Color(0xff6D6D6D);
+  static const Color black = Color(0xff000000);
+}
