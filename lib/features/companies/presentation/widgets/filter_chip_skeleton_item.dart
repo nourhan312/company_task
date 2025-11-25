d9709ad09b524385e84757e13668b228efd00549
Nour <nourhanaymanghonim@gmail.com>
2025-11-25 18:23:36 +0200

    feat: add FilterChipSkeletonItem widget for loading states

---
+++ lib/features/companies/presentation/widgets/filter_chip_skeleton_item.dart
@@ -0,0 +1,21 @@
+import 'package:company_task/core/extensions/context_extensions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class FilterChipSkeletonItem extends StatelessWidget {
+  const FilterChipSkeletonItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: context.symmetric(horizontal: 22.w, vertical: 8.h),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.r),
+        border: Border.all(color: Colors.grey.shade300),
+      ),
+      child: Bone.text(words: 1, fontSize: 14.sp),
+    );
+  }
+}
